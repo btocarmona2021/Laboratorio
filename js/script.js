c28d3fed0a5fed66666f11e309608f14771ebb47
Alberto C <btocarmona@gmail.com>
2024-10-28 01:27:20 -0300

    update responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 const ayuda = document.getElementById('ayuda');
+const nombre = document.getElementById('tdnombre');
+const apellido = document.getElementById('tdapellido');
+const email = document.getElementById('tdemail');
+const telefono = document.getElementById('tdtelefono');
+const edad = document.getElementById('tdedad');
+const direccion = document.getElementById('tddireccion');
+const provincia = document.getElementById('tdprovincia');
+const codigopostal = document.getElementById('tdcodigopostal');
+const contacto = document.getElementById('tdcontacto');
+const suscripcion = document.getElementById('tdsuscripcion');
+
 function quitarAyuda() {
-    ayuda.style.display="none";
+    ayuda.style.display = "none";
+}
+
+function rellenaCampo(name, valor) {
+    const elemento = document.getElementById('td' + name);
+    if (name === 'suscripcion') {
+        elemento.innerText += ' ' + valor;
+        return;
+    }
+    elemento.innerText = valor;
 }
